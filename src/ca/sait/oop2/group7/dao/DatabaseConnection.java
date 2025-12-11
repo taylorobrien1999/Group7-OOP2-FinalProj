9688b33ba148528bcf8a3c2b497c344b3a6dd12f
Alexander-Pacunayen <alexander.pacunayen@edu.sait.ca>
2025-12-11 16:14:32 -0700

    Update DatabaseConnection.java For reference on how to do your Data base connection

--- src/ca/sait/oop2/group7/dao/DatabaseConnection.java
+++ src/ca/sait/oop2/group7/dao/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 	// TODO: change these fields to url, user and pass when DB is ready
-	private static final String URL = "tbd";
-	private static final String USER = "root";
-	private static final String PASSWORD = "password";
+	private static final String URL = "jdbc:mariadb://localhost:3306/testdb";
+	private static final String USER = "libraryuser";
+	private static final String PASSWORD = "yournewpassword";
 			
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USER, PASSWORD);
