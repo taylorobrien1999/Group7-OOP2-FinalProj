90ba15a10f207556213969955a7bef2cc06548e9
Alexander-Pacunayen <alexander.pacunayen@edu.sait.ca>
2025-12-11 16:13:11 -0700

    Update BookDao.java Insert table columns are there for you to copy

--- src/ca/sait/oop2/group7/dao/BookDao.java
+++ src/ca/sait/oop2/group7/dao/BookDao.java
@@ -1,5 +1,8 @@
 package ca.sait.oop2.group7.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import ca.sait.oop2.group7.model.Book;
 
@@ -7,6 +10,23 @@ public class BookDao {
 	
 	public void insert (Book book) {
 		// jdbc INSERT into books table
+		
+		String sql = "INSERT INTO books (title, author, isbn, available) VALUES (?,?,?,?)";
+		
+		try (Connection conn = DatabaseConnection.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(sql)) {
+			
+				stmt.setString(1, book.getTitle());
+				stmt.setString(2, book.getAuthor());
+				stmt.setString(3, book.getIsbn());
+				stmt.setBoolean(4, book.isAvailable());
+				
+				
+				stmt.executeUpdate();
+		}catch (SQLException e){
+			e.printStackTrace();
+			
+		}
 	}
 	
 	public List<Book> findAll() {
